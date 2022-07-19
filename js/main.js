9094407b7132042e271456035ebde72aabbe2fba
MD SAHI ALAM KHAN <ksahi4088@gmail.com>
2022-07-19 18:09:16 +0530

    Files uploaded

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $(".banner_img_slider_area").bxSlider({
+    auto: "true",
+    speed: "800",
+    autoStart: "true",
+    pause: "3000",
+  });
+});
+
+document.write("sahil khan");
+window.alert("khan");
+console.log("sahil khan");
+document.getElementById;
+
+const firstNumber = 200;
+const lastNumber = 10;
+console.log(firstNumber / lastNumber);
+
+{
+  let userAge = 23;
+  let requiredAge = 24;
+
+  if (userAge >= requiredAge) {
+    console.log("you can apply!");
+  } else {
+    console.log("You can not apply for jobs!");
+  }
+}
+
+function greet(name) {
+  console.log(name + "happy Birthday");
+}
+greet("sahil");
